0a60698c3c6426d731b81c6b56cea5c692eb9ff0
Brian Nicewander <Brian_Nicewander@hotmail.com>
2023-02-24 10:18:33 -0600

    Add project files.

---
+++ Sales/Sales.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Sales
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Sales))
+        Me.pbxLogo = New System.Windows.Forms.PictureBox()
+        Me.pbxLogo2 = New System.Windows.Forms.PictureBox()
+        Me.grpQuantity = New System.Windows.Forms.GroupBox()
+        Me.txtPackage3 = New System.Windows.Forms.TextBox()
+        Me.txtPackage2 = New System.Windows.Forms.TextBox()
+        Me.txtPackage1 = New System.Windows.Forms.TextBox()
+        Me.lblPackageC = New System.Windows.Forms.Label()
+        Me.lblPackageB = New System.Windows.Forms.Label()
+        Me.lblPackageA = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lbloutput = New System.Windows.Forms.Label()
+        Me.lblDateAndTime = New System.Windows.Forms.Label()
+        Me.lblCompany = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblSales = New System.Windows.Forms.Label()
+        CType(Me.pbxLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxLogo2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpQuantity.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pbxLogo
+        '
+        Me.pbxLogo.Image = CType(resources.GetObject("pbxLogo.Image"), System.Drawing.Image)
+        Me.pbxLogo.Location = New System.Drawing.Point(2, 12)
+        Me.pbxLogo.Name = "pbxLogo"
+        Me.pbxLogo.Size = New System.Drawing.Size(105, 85)
+        Me.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxLogo.TabIndex = 0
+        Me.pbxLogo.TabStop = False
+        '
+        'pbxLogo2
+        '
+        Me.pbxLogo2.Image = CType(resources.GetObject("pbxLogo2.Image"), System.Drawing.Image)
+        Me.pbxLogo2.Location = New System.Drawing.Point(444, 12)
+        Me.pbxLogo2.Name = "pbxLogo2"
+        Me.pbxLogo2.Size = New System.Drawing.Size(105, 85)
+        Me.pbxLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxLogo2.TabIndex = 1
+        Me.pbxLogo2.TabStop = False
+        '
+        'grpQuantity
+        '
+        Me.grpQuantity.Controls.Add(Me.txtPackage3)
+        Me.grpQuantity.Controls.Add(Me.txtPackage2)
+        Me.grpQuantity.Controls.Add(Me.txtPackage1)
+        Me.grpQuantity.Controls.Add(Me.lblPackageC)
+        Me.grpQuantity.Controls.Add(Me.lblPackageB)
+        Me.grpQuantity.Controls.Add(Me.lblPackageA)
+        Me.grpQuantity.Location = New System.Drawing.Point(179, 117)
+        Me.grpQuantity.Name = "grpQuantity"
+        Me.grpQuantity.Size = New System.Drawing.Size(220, 141)
+        Me.grpQuantity.TabIndex = 2
+        Me.grpQuantity.TabStop = False
+        Me.grpQuantity.Text = "Quantity"
+        '
+        'txtPackage3
+        '
+        Me.txtPackage3.Location = New System.Drawing.Point(86, 101)
+        Me.txtPackage3.Name = "txtPackage3"
+        Me.txtPackage3.Size = New System.Drawing.Size(100, 20)
+        Me.txtPackage3.TabIndex = 5
+        '
+        'txtPackage2
+        '
+        Me.txtPackage2.Location = New System.Drawing.Point(86, 61)
+        Me.txtPackage2.Name = "txtPackage2"
+        Me.txtPackage2.Size = New System.Drawing.Size(100, 20)
+        Me.txtPackage2.TabIndex = 4
+        '
+        'txtPackage1
+        '
+        Me.txtPackage1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.txtPackage1.Location = New System.Drawing.Point(86, 26)
+        Me.txtPackage1.Name = "txtPackage1"
+        Me.txtPackage1.Size = New System.Drawing.Size(100, 20)
+        Me.txtPackage1.TabIndex = 3
+        '
+        'lblPackageC
+        '
+        Me.lblPackageC.AutoSize = True
+        Me.lblPackageC.Location = New System.Drawing.Point(6, 104)
+        Me.lblPackageC.Name = "lblPackageC"
+        Me.lblPackageC.Size = New System.Drawing.Size(63, 13)
+        Me.lblPackageC.TabIndex = 2
+        Me.lblPackageC.Text = "Package C:"
+        '
+        'lblPackageB
+        '
+        Me.lblPackageB.AutoSize = True
+        Me.lblPackageB.Location = New System.Drawing.Point(6, 68)
+        Me.lblPackageB.Name = "lblPackageB"
+        Me.lblPackageB.Size = New System.Drawing.Size(63, 13)
+        Me.lblPackageB.TabIndex = 1
+        Me.lblPackageB.Text = "Package B:"
+        '
+        'lblPackageA
+        '
+        Me.lblPackageA.AutoSize = True
+        Me.lblPackageA.Location = New System.Drawing.Point(6, 29)
+        Me.lblPackageA.Name = "lblPackageA"
+        Me.lblPackageA.Size = New System.Drawing.Size(63, 13)
+        Me.lblPackageA.TabIndex = 0
+        Me.lblPackageA.Text = "Package A:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(363, 316)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "&Calculate"
+        Me.ToolTip1.SetToolTip(Me.btnCalculate, "This will Calculate your total")
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(272, 374)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "C&lear"
+        Me.ToolTip1.SetToolTip(Me.btnClear, "This will Clear your inputs and results")
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(465, 374)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "&Exit"
+        Me.ToolTip1.SetToolTip(Me.btnExit, "This will Exit you out of the Program")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lbloutput
+        '
+        Me.lbloutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbloutput.Location = New System.Drawing.Point(12, 299)
+        Me.lbloutput.Name = "lbloutput"
+        Me.lbloutput.Size = New System.Drawing.Size(254, 131)
+        Me.lbloutput.TabIndex = 3
+        '
+        'lblDateAndTime
+        '
+        Me.lblDateAndTime.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblDateAndTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDateAndTime.Location = New System.Drawing.Point(330, 410)
+        Me.lblDateAndTime.Name = "lblDateAndTime"
+        Me.lblDateAndTime.Size = New System.Drawing.Size(219, 25)
+        Me.lblDateAndTime.TabIndex = 7
+        Me.lblDateAndTime.Text = "Date and Time"
+        '
+        'lblCompany
+        '
+        Me.lblCompany.Font = New System.Drawing.Font("Vivaldi", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCompany.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.lblCompany.Location = New System.Drawing.Point(113, 12)
+        Me.lblCompany.Name = "lblCompany"
+        Me.lblCompany.Size = New System.Drawing.Size(325, 85)
+        Me.lblCompany.TabIndex = 8
+        Me.lblCompany.Text = "Ice Wolf hotel Package Sales"
+        Me.lblCompany.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(458, 441)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(91, 13)
+        Me.lblName.TabIndex = 9
+        Me.lblName.Text = "Brian Nicewander"
+        '
+        'lblSales
+        '
+        Me.lblSales.AutoSize = True
+        Me.lblSales.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSales.Location = New System.Drawing.Point(123, 77)
+        Me.lblSales.Name = "lblSales"
+        Me.lblSales.Size = New System.Drawing.Size(303, 20)
+        Me.lblSales.TabIndex = 10
+        Me.lblSales.Text = "Calculate the total of packages price"
+        '
+        'Sales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(552, 459)
+        Me.Controls.Add(Me.lblSales)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblCompany)
+        Me.Controls.Add(Me.lblDateAndTime)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lbloutput)
+        Me.Controls.Add(Me.grpQuantity)
+        Me.Controls.Add(Me.pbxLogo2)
+        Me.Controls.Add(Me.pbxLogo)
+        Me.Name = "Sales"
+        Me.Text = "Package Sales"
+        CType(Me.pbxLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxLogo2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpQuantity.ResumeLayout(False)
+        Me.grpQuantity.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pbxLogo As PictureBox
+    Friend WithEvents pbxLogo2 As PictureBox
+    Friend WithEvents grpQuantity As GroupBox
+    Friend WithEvents txtPackage3 As TextBox
+    Friend WithEvents txtPackage2 As TextBox
+    Friend WithEvents txtPackage1 As TextBox
+    Friend WithEvents lblPackageC As Label
+    Friend WithEvents lblPackageB As Label
+    Friend WithEvents lblPackageA As Label
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents lbloutput As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblDateAndTime As Label
+    Friend WithEvents lblCompany As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblSales As Label
+End Class
---
+++ Sales/Sales.vb
@@ -0,0 +1,167 @@
+﻿Public Class Sales
+    'Project Sales
+    'Application for Package Sales
+    'Programmed by Brian Nicewander
+    'Date last modified 2/26/19
+
+    'Declare inputs
+    Dim intPackage1 As Integer    'Package A
+    Dim intPackage2 As Integer    'Package B
+    Dim intPackage3 As Integer    'Package C
+
+    'Variables for output
+    Dim dblPackageA As Double
+    Dim dblPackageB As Double
+    Dim dblPackageC As Double
+    Dim dblGrandTotal As Double
+
+    'Constant for Discounts
+    Const decDiscountA As Decimal = CDec(0.8)         '10-19 packages 20% discount
+    Const decDiscountB As Decimal = CDec(0.7)         '20-49 packages 30% discount
+    Const decDiscountC As Decimal = CDec(0.6)         '50-99 Packages 40% discount
+    Const decDiscountD As Decimal = CDec(0.5)         '100 or more packages 50% discount
+
+    'Constant for package cost
+    Const intPackageA As Integer = 99           'Price for package A
+    Const intPackageB As Integer = 199          'Price for package B
+    Const intPackageC As Integer = 299          'Price for Package C
+
+    Private Sub Sales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Shows the date and time in a label
+        lblDateAndTime.Text = Now.ToString("D") & " " & Now.ToString("T")
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Calculate the total of packages and the discount offered and displays total of each package and grand total in a label.
+
+        'Get the number of Package A 
+        If Integer.TryParse(txtPackage1.Text, intPackage1) Then
+            'Validate a number of 0 or higher in Package A
+            If intPackage1 >= 0 Then
+
+                'Calculating the price of Pacakge A with no discount
+                dblPackageA = intPackage1 * intPackageA
+
+                'Discount for Package A
+                Select Case intPackage1
+                    Case 10 To 19
+                        dblPackageA = (intPackage1 * intPackageA) * decDiscountA
+                    Case 20 To 49
+                        dblPackageA = (intPackage1 * intPackageA) * decDiscountB
+                    Case 50 To 99
+                        dblPackageA = (intPackage1 * intPackageA) * decDiscountC
+                    Case Is >= 100
+                        dblPackageA = (intPackage1 * intPackageA) * decDiscountD
+                End Select
+
+            Else
+                'Error: If a Integer of 0 or higher is not entered
+                MessageBox.Show("Please enter a Integer of 0 or higher in Package A!")
+                txtPackage1.Focus()
+                Exit Sub
+            End If
+
+        Else
+            'Error: If the text box is left empty or if they put text in that is not a Integer
+            MessageBox.Show("Please enter a valid Integer in Package A!")
+            txtPackage1.Focus()
+            Exit Sub
+        End If
+
+        'Get the number of Package B
+        If Integer.TryParse(txtPackage2.Text, intPackage2) Then
+            'Validate a number of 0 or higher in Package B
+            If intPackage2 >= 0 Then
+
+                'Calculating the price of Pacakge B with no discount
+                dblPackageB = intPackage2 * intPackageB
+
+                'Discount for Package B
+                Select Case intPackage2
+                    Case 10 To 19
+                        dblPackageB = (intPackage2 * intPackageB) * decDiscountA
+                    Case 20 To 49
+                        dblPackageB = (intPackage2 * intPackageB) * decDiscountB
+                    Case 50 To 99
+                        dblPackageB = (intPackage2 * intPackageB) * decDiscountC
+                    Case Is >= 100
+                        dblPackageB = (intPackage2 * intPackageB) * decDiscountD
+                End Select
+
+            Else
+                'Error: If a Integer of 0 or higher is not entered
+                MessageBox.Show("Please enter a Integer of 0 or higher in Package B!")
+                txtPackage2.Focus()
+                Exit Sub
+            End If
+
+        Else
+            'Error: If the text box is left empty or if they put text in that is not a Integer
+            MessageBox.Show("Please enter a valid Integer in Package B!")
+            txtPackage2.Focus()
+            Exit Sub
+        End If
+
+        'Get the number of Package C
+        If Integer.TryParse(txtPackage3.Text, intPackage3) Then
+            'Validate a number of 0 or higher in Package C
+            If intPackage3 >= 0 Then
+
+                'Calculating the price of Pacakge C with no discount
+                dblPackageC = intPackage3 * intPackageC
+
+                'Discount for Package C
+                Select Case intPackage3
+                    Case 10 To 19
+                        dblPackageC = (intPackage3 * intPackageC) * decDiscountA
+                    Case 20 To 49
+                        dblPackageC = (intPackage3 * intPackageC) * decDiscountB
+                    Case 50 To 99
+                        dblPackageC = (intPackage3 * intPackageC) * decDiscountC
+                    Case Is >= 100
+                        dblPackageC = (intPackage3 * intPackageC) * decDiscountD
+                End Select
+
+            Else
+                'Error: If a Integer of 0 or higher is not entered
+                MessageBox.Show("Please enter a Integer of 0 or higher in Package C!")
+                txtPackage3.Focus()
+                Exit Sub
+            End If
+
+        Else
+            'Error: If the text box is left empty or if they put text in that is not a Integer
+            MessageBox.Show("Please enter a valid Integer in Package C!")
+            txtPackage3.Focus()
+            Exit Sub
+        End If
+
+
+        'Calculate Grand total
+        dblGrandTotal = dblPackageC + dblPackageB + dblPackageA
+
+        'Display fees
+        lbloutput.Text = "Package A:" + dblPackageA.ToString("C") & vbCrLf & "Package B: " + dblPackageB.ToString("C") & vbCrLf & "Package C:" + dblPackageC.ToString("C") & vbCrLf & vbCrLf & "Grand Total: " + dblGrandTotal.ToString("C")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the text box for number of packages
+        txtPackage1.Clear()
+        txtPackage2.Clear()
+        txtPackage3.Clear()
+
+        'Clear the fee lable
+        lbloutput.Text = String.Empty
+
+        'Resets the time and date
+        lblDateAndTime.Text = Now.ToString("D") & " " & Now.ToString("T")
+
+        'Give pacakge A fucus
+        txtPackage1.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form.
+        Me.Close()
+    End Sub
+End Class
